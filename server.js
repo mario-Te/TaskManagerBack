a52990e52fd4f145ff94f90f16d3930cd9d450d6
mario-Te <mariothoumy@036.com>
2025-07-01 15:46:54 +0300

    edit website cookie3

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(
   cors({
     origin: "https://task-manager-front-delta.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(cookieParser());
